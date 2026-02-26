c1c8a4cdbda4f7eef048dd197e3a935f91056b3b
Krishna Paliwal <krishna.plwl264@gmail.com>
2026-02-26 14:59:08 +0530

    Deployment bugs fixed again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://coqatalyst.github.io/',
-  base: '/website',
+  base: '/website/',
   redirects: {
     '/blog': '/under-construction',
     '/contact': '/under-construction',
