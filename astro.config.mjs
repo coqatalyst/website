9e22b9618751cc868224f03cb5965c86ae9b4f33
Krishna Paliwal <krishna.plwl264@gmail.com>
2026-02-26 13:54:51 +0530

    First version

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  redirects: {
+    '/blog': '/under-construction',
+    '/contact': '/under-construction',
+    '/about': '/under-construction',
+  }
+});
