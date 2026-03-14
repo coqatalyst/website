bea8eef7cc7f69db3f64aac525253a6ab824b920
Krishna Paliwal <krishna.plwl264@gmail.com>
2026-03-14 20:00:45 +0530

    update socials

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -2,22 +2,19 @@
 
 export const SOCIALS = {
   DISCORD: {
-    label: 'Discord',
-    href: 'https://discord.gg/8gyA2E3a',
-    value: 'Join our server',
-    accent: '#226d0b',
-    rgb: '34,109,11',
+    label: "Discord",
+    href: "https://discord.gg/rmRRZ6HMVM",
+    value: "Join our server",
+    accent: "#226d0b",
+    rgb: "34,109,11",
   },
   EMAIL: {
-    label: 'Email',
-    href: 'mailto:coqatalyst@gmail.com',
-    value: 'coqatalyst@gmail.com',
-    accent: '#dfa651',
-    rgb: '223,166,81',
+    label: "Email",
+    href: "mailto:coqatalyst@gmail.com",
+    value: "coqatalyst@gmail.com",
+    accent: "#dfa651",
+    rgb: "223,166,81",
   },
 };
 
-export const CONTACT_METHODS = [
-  SOCIALS.DISCORD,
-  SOCIALS.EMAIL,
-];
+export const CONTACT_METHODS = [SOCIALS.DISCORD, SOCIALS.EMAIL];
